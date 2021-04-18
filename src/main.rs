906c0430b546fad2867e5939b9271fc4a03cc970
jo! <jo@pop-os.local>
2021-04-18 20:21:00 +0100

    IT WORKS KINDA

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,112 @@
 use std::io::Read;
-use std::process::{Command, Output};
+use std::process::{Command, Output, exit};
 use std::io;
+use std::env;
+use directories::{BaseDirs, UserDirs, ProjectDirs};
+use std::fs;
+use std::path::Path;
+use toml::value::Array;
+use serde_derive::Deserialize;
+
+#[derive(Deserialize)]
+struct Config {
+    subscriptions: Array<>,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
+
+    // SUB BOX
+    let project = ProjectDirs::from("com", "j0lol", "rs-youtube").unwrap();
+    let mut config_dir = project.config_dir();
+
+    fs::create_dir_all(config_dir)?;
+
+    let config_path = format!("{}/config.toml",config_dir.to_str().unwrap());
+    config_dir = std::path::Path::new(&config_path);
+
+    let config: Config = toml::from_str(&fs::read_to_string(config_dir)?).unwrap();
+
     let client = reqwest::Client::new();
-    let mut res = client.post("https://www.youtube.com/youtubei/v1/browse?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8")
-        .body(r#"{
-  "context": {
-    "client": {
-      "clientName": "WEB",
-      "clientVersion": "2.20201210.01.00",
-      "originalUrl": "https://www.youtube.com/",
-      "platform": "DESKTOP",
-      "clientFormFactor": "UNKNOWN_FORM_FACTOR",
-      "newVisitorCookie": true
+
+    for i in 0..config.subscriptions.len() {
+        let res = request_browse(&client, config.subscriptions[i].as_str().unwrap()).unwrap();
+        let channel_name: &serde_json::Value = &res["header"]["c4TabbedHeaderRenderer"]["title"];
+        let channel_name = channel_name.as_str().unwrap();
+
+        // Get channel name
+        //let channel_subs: &serde_json::Value = &res["header"]["c4TabbedHeaderRenderer"]["subscriberCountText"]["simpleText"];
+        //let channel_subs = channel_subs.as_str().unwrap();
+        let video_name: &serde_json::Value = &res["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][1]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][0]["itemSectionRenderer"]["contents"][0]["gridRenderer"]["items"][0]["gridVideoRenderer"]["title"]["runs"][0]["text"];
+        let video_name = video_name.as_str().unwrap();
+        // Get timestamp
+        let video_timestamp: &serde_json::Value = &res["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][1]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][0]["itemSectionRenderer"]["contents"][0]["gridRenderer"]["items"][0]["gridVideoRenderer"]["publishedTimeText"]["simpleText"];
+        let video_timestamp = video_timestamp.as_str().unwrap();
+
+        // Print channel name
+        println!("{}) {} {}\n{}\n",i, channel_name, video_timestamp, video_name);
+
+
     }
-  },
-  "browseId": "UCRC6cNamj9tYAO6h_RXd5xA",
-  "params": "EgZ2aWRlb3M%3D"
- }"#)
-        .send()?;
+    println!("Type in a channel's number to open it");
+
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let input: usize = input.trim().parse().expect("Please type a number!");
+    show_channel(config.subscriptions[input].as_str().unwrap());
+    Ok(())
+}
+
+// Make a /browse request
+fn request_browse(client: &reqwest::Client, channel_id: &str) -> Option<serde_json::Value> {
+    let mut res = client.post("https://www.youtube.com/youtubei/v1/browse?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8")
+        .body(format!("{{\r\n  \"context\": {{\r\n    \"client\": {{\r\n      \"clientName\": \"WEB\",\r\n      \"clientVersion\": \"2.20201210.01.00\",\r\n      \"originalUrl\": \"https://www.youtube.com/\",\r\n      \"platform\": \"DESKTOP\",\r\n      \"clientFormFactor\": \"UNKNOWN_FORM_FACTOR\",\r\n      \"newVisitorCookie\": true\r\n    }}\r\n  }},\r\n  \"browseId\": \"{}\",\r\n  \"params\": \"EgZ2aWRlb3M%3D\"\r\n }}", channel_id)
+        )
+        .send().ok()?;
 
 
     let mut body = String::new();
-    res.read_to_string(&mut body)?;
-    println!("Status: {}", res.status());
-    //println!("Headers:\n{:#?}", res.headers());
-    //println!("Body:\n{}", body);
-    // let mut res = reqwest::get("https://www.youtube.com/youtubei/v1/browse?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8")?;
-    // let mut body = String::new();
-    // res.read_to_string(&mut body)?;
-    //
-    // println!("Status: {}", res.status());
-    // println!("Headers:\n{:#?}", res.headers());
-    // println!("Body:\n{}", body);
+    res.read_to_string(&mut body).ok()?;
 
-    let res: serde_json::Value = serde_json::from_str(&body).expect("Unable to parse");
+    Some(serde_json::from_str(&body).expect("Unable to parse"))
+}
+
+// Make a link made of two parts
+fn make_url(prefix: &str, suffix: &str) -> String {
+    [prefix, suffix].join("")
+}
+
+// Run a command on windows, linux or mac
+fn run_command(command: &str) -> Output {
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(&["/C", command])
+            .output()
+            .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+            .arg("-c")
+            .arg(command)
+            .output()
+            .expect("failed to execute process")
+    };
+    output
+}
+
+fn show_channel(channel_id: &str) {
+
+    let client = reqwest::Client::new();
+
+    // let args: Vec<String> = env::args().collect();
+    //
+    // let channel_id = &args[1];
+
+    let res = request_browse(&client, channel_id).unwrap();
     let channel_name: &serde_json::Value = &res["header"]["c4TabbedHeaderRenderer"]["title"];
     let channel_name = channel_name.as_str().unwrap();
 
@@ -44,10 +114,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let channel_subs: &serde_json::Value = &res["header"]["c4TabbedHeaderRenderer"]["subscriberCountText"]["simpleText"];
     let channel_subs = channel_subs.as_str().unwrap();
 
+    // Render channel banner
+    println!("{}\n{}\n",channel_name,channel_subs);
     let mut arr:[&str;30] = ["null";30];
 
     for i in 0..30 {
-    // Get ID
+        // Get ID
         let video_id: &serde_json::Value = &res["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][1]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][0]["itemSectionRenderer"]["contents"][0]["gridRenderer"]["items"][i]["gridVideoRenderer"]["videoId"];
         let video_id = video_id.as_str().unwrap();
 
@@ -55,8 +127,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let video_name: &serde_json::Value = &res["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][1]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][0]["itemSectionRenderer"]["contents"][0]["gridRenderer"]["items"][i]["gridVideoRenderer"]["title"]["runs"][0]["text"];
         let video_name = video_name.as_str().unwrap();
 
+        // Get timestamp
+        let video_timestamp: &serde_json::Value = &res["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][1]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][0]["itemSectionRenderer"]["contents"][0]["gridRenderer"]["items"][i]["gridVideoRenderer"]["publishedTimeText"]["simpleText"];
+        let video_timestamp = video_timestamp.as_str().unwrap();
+
+
         arr[i] = video_id;
-        println!("{}) {} by {} — https://youtube.com/watch?v={}, {}", i, video_name, channel_name, video_id, channel_subs);
+        // Render video list
+        println!("{}) {}  {}", i, video_name, video_timestamp);
 
     }
     // Print
@@ -77,24 +155,4 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Run MPV command
     run_command(&command);
-    Ok(())
-}
-
-fn make_url(prefix: &str, suffix: &str) -> String {
-    [prefix, suffix].join("")
-}
-fn run_command(command: &str) -> Output {
-    let output = if cfg!(target_os = "windows") {
-        Command::new("cmd")
-            .args(&["/C", command])
-            .output()
-            .expect("failed to execute process")
-    } else {
-        Command::new("sh")
-            .arg("-c")
-            .arg(command)
-            .output()
-            .expect("failed to execute process")
-    };
-    output
 }
