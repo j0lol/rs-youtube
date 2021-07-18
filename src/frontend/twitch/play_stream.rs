77fd81885f73a47a36d2351ed27f1fc16ac39342
jo! <24716467+j0lol@users.noreply.github.com>
2021-07-18 16:53:32 +0100

    Update play_stream.rs

--- src/frontend/twitch/play_stream.rs
+++ src/frontend/twitch/play_stream.rs
@@ -5,13 +5,6 @@ pub fn play_stream(channel_name: String) {
     let url = format!("https://twitch.tv/{}", channel_name);
     let player = load_config().unwrap();
 
-    let chat_url= format!("{}{}", url, "/chat");
-
-    match webbrowser::open(&chat_url) {
-        Err(_) => {println!("Failed to open chat in browser...")}
-        _ => {println!("Opening chat in browser...")}
-    }
-
     let player = &(*player.options.video_player.as_str().unwrap());
     println!("▶ Now playing: {} :)", url);
     run_command(&format!("{} {}", player, url));
